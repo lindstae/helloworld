f82c23d884ef9984f0b122a83d21809e5acb0bf8
Sebastian Lindstaedt <az400sli@outlook.com>
2025-02-09 16:12:42 +0100

    Add PowerShell script to aggregate .ex file contents and copy to clipboard

---
+++ diverse/filecontents.ps1
@@ -0,0 +1,17 @@
+﻿# Pfad zum übergeordneten Ordner anpassen
+$folderPath = "C:\Users\Sebastian\source\elixir\repos\helloworld\"
+
+# Alle .ex-Dateien rekursiv ermitteln
+$files = Get-ChildItem -Path $folderPath -Filter *.ex* -Recurse
+
+# Inhalte der Dateien zusammenfassen.
+# Die foreach-Schleife wird in einen Subexpression-Block $() eingeschlossen, 
+# damit ihre Ausgabe als Pipelineobjekte zur weiteren Verarbeitung (hier Out-String) geliefert wird.
+$content = $(foreach ($file in $files) {
+    "# Datei: $($file.FullName)"
+    Get-Content -Path $file.FullName
+    ""  # Leerzeile als Trenner
+}) | Out-String
+
+# Den zusammengesetzten Text in die Zwischenablage kopieren
+$content | Set-Clipboard
